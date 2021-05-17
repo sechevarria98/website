cef43ff9d0a65879bbd81f3c7a5e6c346b12311d
sechevarria98 <samechevarria2@gmail.com>
2021-05-17 15:31:51 -0500

    the contact page

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../App.css';
+
+const Contact = () => {
+    return (
+        <div className="page">
+            CONTACT PAGE
+        </div>
+
+    );  
+}
+
+export default Contact;
