de3d44c452ed8040fc274dd7bb8e4189d9afb01b
sechevarria98 <samechevarria2@gmail.com>
2021-05-17 19:11:49 -0500

    almost finished with the contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import '../../App.css';
 
+import LinkedIn from '../../img/linkedin.svg';
+import Github from '../../img/github.svg';
+import Email from '../../img/envelope-square-solid.svg';
+import Phone from '../../img/phone-solid.svg';
+
 const Contact = () => {
+
+    const contacts = {
+        'email': { link: "samechevarria2@gmail.com", image: Email },
+        'phone': { link: "7739541626", image: Phone },
+        'github': { link: "https://github.com/sechevarria98", image: Github},
+        'linkedin': { link: " https://www.linkedin.com/in/samuel-echevarria-793a80199/", image: LinkedIn }
+    }
+
     return (
         <div className="page">
             <h2>CONTACT</h2>
+
+            <section className = "cardlist">
+                {Object.entries(contacts).map(( [contact, {link, image}]) => (
+                    <article className = "card">
+                        <header className = "cardheader">
+                            <h2> <img src = {image} alt = {contact + "logo"} />{ contact }</h2>
+                            <p>{link}</p>
+                        </header>
+                    </article> 
+                ))}
+
+            </section>
+
         </div>
 
     );  
