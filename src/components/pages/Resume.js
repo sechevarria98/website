420eee971b558e7e552ac87f6d4b5985cf046904
sechevarria98 <samechevarria2@gmail.com>
2021-05-18 16:44:22 -0500

    changing file path for pdf

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -18,7 +18,7 @@ const Resume = () => {
             <h2>RESUME</h2>
             <Document
                 className = "docs"
-                file="/assets/docs/Echevarria_Resume.pdf"
+                file="/website/blob/gh-pages/assets/docs/Echevarria_Resume.pdf"
                 onLoadSuccess={onDocumentLoadSuccess}
             >
                 <Page pageNumber={pageNumber} />
